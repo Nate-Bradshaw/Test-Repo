6aab7dca856c851123241b1f333f87ca54311461
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-11-11 13:48:07 +0000

    fixed first part of the (yet to be) function

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function Transposition(keys, transposition_amount){
 
 //get list of notes (for now input directly into the code)
 let notes = [11, 7, 2, 3, 2];
-console.log(notes);
+console.log(notes, "start list");
 
 notes.sort(function(a, b){return a-b});
-console.log(notes);
+console.log(notes, "sorted list");
 
 let uniqueNotes = [...new Set(notes)];
 notes = uniqueNotes;
-console.log(notes);
+console.log(notes, "sorted list with repeats removed");
 
 
 //pop() removes last, shift() removes first, Math.abs() to negative to pos
@@ -25,27 +25,31 @@ for (let i = 0; i < arLength; i++) {
     let bestDifference = 13; //bigger than any dif could be
     let tempNotes = notes;
     //let outputNotes = tempNotes; //put elsware? (methinks after the itteration)
-    for (let j = 0; j < i; j++){
-        let tempInt = tempNotes.shift(j);
-        tempNotes.push(tempInt);
-    }
-    let tempTempNotes = tempNotes;
-    let start = tempNotes.pop();
-    let end = tempNotes.shift();
+
+    let tempInt = tempNotes.shift(i);
+    tempNotes.push(tempInt);
+    console.log(tempNotes)
+
+    //let tempTempNotes = tempNotes;
+    //let start = tempNotes.pop();
+    //let end = tempNotes.shift();
+    
+    /*
     let tempDifference = end - start; //this is wrong, the diffence loops around
     if (tempDifference < 0){
         tempDifference = Math.abs(tempDifference);
     }
+
     if (tempDifference < bestDifference){
         bestDiffernce = tempDifference;
         bestNotes = tempTempNotes; //not outputting anything idk why, fix
     }
     //add tiebreaker statement
+    */
 }
-console.log(bestNotes, bestDiffernce);
+//console.log(bestNotes, bestDiffernce);
 
-//2  11   9
-//11 7    4 not -4
+//diff counting up so 7 >> 3 isnt 4 its 8 as 7-8-9-10-11-0-1-2-3 is 8 steps. itteration ofc
 //(either in 0-11 form or convert)
 //normal form (remove duplicates, order and find smallest difference between first and last)
 //invert, find prime form (order and find smallest difference between first and last)
