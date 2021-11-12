8c91c97c09dd171bed4d0a0dbed48c6fd1ede89f
Nate Bradshaw <nate.bradshaw5959@gmail.com>
2021-11-12 10:46:18 +0000

    tiebreaker statement working but normal form reconstruction needs fix

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//console.log("'ello wurld");
-/*
-function Transposition(keys, transposition_amount){
-    out = keys + transposition_amount;
-    return(out);
-}
-*/
-
-
-
 function FindDifference(start, end){ //returns differnce
     differnce = 0;
     start1 = start;
@@ -24,69 +14,88 @@ function FindDifference(start, end){ //returns differnce
     return differnce;
 }
 
+function TieBreak(bestNotes, duplicates, length){
+    notesLength = length.push();
+    console.log(notesLength);
+    for (let i = 0; i < duplicates+1; i++){
+        //new code to split based on how many tiebreakers there are
+        tempNotes = bestNotes.slice(notesLength * i, notesLength * (i+1));
+        storedDifference = 13;
+
+        tempPop = tempNotes.pop();
+        penultimate = tempNotes.pop();
+        tempNotes.push(tempPop);
+        tempNotes.push(penultimate);
+        start = tempNotes.shift();
+        tempNotes.unshift(start);
+        console.log(start, penultimate, "1st and penultimate");
+
+        normalForm = [];
+        differnce = FindDifference(start, penultimate); //start and penultimate note to compare
+        if (differnce < storedDifference && differnce != storedDifference){
+            storedDifference = differnce;
+            normalForm = normalForm.concat(tempNotes);
+            duplication++;
+        }    
+        //if there is ANOTHER tie breaker, the lowest starting number is used
+    }
+    console.log(normalForm);
+    if (duplication > 1){
+        //get first note and compare for FINAL winner;
+    }
+
+    //return finalNotes; //endpoint
+}
+
+function NormalForm(notes){
+    notes.sort(function(a, b){return a-b});
+    console.log(notes, "sorted list");
+    
+    uniqueNotes = [...new Set(notes)];
+    notes = uniqueNotes;
+    console.log(notes, "sorted list with repeats removed");
+    
+    //pop() removes last, shift() removes first, Math.abs() to negative to pos
+    arLength = notes.length;
+    storedDifference = 13;
+    storedNotes = [];
+    duplication = -1; //-1 out means code has gone wrong
+    //let tempNotes = notes;
+    
+    //get first and last values, compare distance (temp arr?) and repeat foreach combo
+    for (let i = 0; i < arLength; i++) {
+        tempNotes = notes;
+    
+        tempInt = tempNotes.shift(i);
+        tempNotes.push(tempInt);
+        console.log(tempNotes)
+    
+        end = tempNotes.pop();
+        tempNotes.push(end);
+        start = tempNotes.shift();
+        tempNotes.unshift(start);
+        console.log(start, end, "1st and last");
+        
+        differnce = FindDifference(start, end); //works now
+        if (differnce < storedDifference && differnce != storedDifference){
+            storedDifference = differnce;
+            storedNotes = storedNotes.concat(tempNotes);
+            duplication++;
+        }
+    
+        console.log(differnce, "diff"); 
+    } 
+    console.log(storedDifference, "diff", storedNotes, "notes", duplication, "dupe");
+    normalForm = TieBreak(storedNotes, duplication, uniqueNotes);
+}
 //get list of notes (for now input directly into the code)
 let notes = [11, 7, 2, 3, 2];
 console.log(notes, "start list");
 
-notes.sort(function(a, b){return a-b});
-console.log(notes, "sorted list");
-
-let uniqueNotes = [...new Set(notes)];
-notes = uniqueNotes;
-console.log(notes, "sorted list with repeats removed");
+normalFormFinal = NormalForm(notes);
 
 
-//pop() removes last, shift() removes first, Math.abs() to negative to pos
-let arLength = notes.length;
-let storedDifference = 13;
-let storedNotes = [];
-let duplication = 0;
-//let tempNotes = notes;
-
-//get first and last values, compare distance (temp arr?) and repeat foreach combo
-for (let i = 0; i < arLength; i++) {
-    tempNotes = notes;
-
-    tempInt = tempNotes.shift(i);
-    tempNotes.push(tempInt);
-    console.log(tempNotes)
-
-    end = tempNotes.pop();
-    tempNotes.push(end);
-    start = tempNotes.shift();
-    tempNotes.unshift(start);
-    console.log(start, end, "1st and last");
-    
-    /*
-    let tempDifference = end - start; //this is wrong, the diffence loops around
-    if (tempDifference < 0){
-        tempDifference = Math.abs(tempDifference);
-    }
-    */
-    differnce = FindDifference(start, end); //works now
-    if (differnce < storedDifference && differnce != storedDifference){
-        storedDifference = differnce;
-        storedNotes = tempNotes;
-    }
-    else if (differnce == storedDifference){
-        console.log(storedNotes, tempNotes);
-        debugList = storedNotes.concat(tempNotes); //puts out 2 of the same, that being the latter list t=with 8 diff, fix.
-        duplication++;
-    }
-
-    console.log(differnce, "diff"); 
-
-    /*
-    if (tempDifference < bestDifference){
-        bestDiffernce = tempDifference;
-        bestNotes = tempTempNotes; //not outputting anything idk why, fix
-    }
-    //add tiebreaker statement
-    */
-}
-
-console.log(storedDifference, "diff", storedNotes, "notes", duplication, "dupe");
-console.log(debugList);
+//console.log(debugList);
 //console.log(bestNotes, bestDiffernce);
 
 //diff counting up so 7 >> 3 isnt 4 its 8 as 7-8-9-10-11-0-1-2-3 is 8 steps. itteration ofc
